57044a4a9c362689cccc7670437cf287bdc9ba94
dronsingh <flydron1148@gmail.com>
2025-07-31 17:05:11 +0000

    saving it

---
+++ basics/var.js
@@ -0,0 +1,10 @@
+const accid=13000222002
+let accemail="flydron1148@gmail.com"
+var accpswrd="12345"
+accCity="daltonganj"
+console.log(accid);
+console.table([accid,accemail,accpswrd,accCity])
+console.log(typeof null);
+console.log(typeof accCity);
+
+
